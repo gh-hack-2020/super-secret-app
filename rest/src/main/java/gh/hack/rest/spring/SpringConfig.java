dc4929d36ba956ad49431558161dc8b20fb76460
Pranav Saxena <psaxena130@gmail.com>
2020-09-15 21:18:48 +0530

    some changes in the base of app

--- rest/src/main/java/gh/hack/rest/spring/SpringConfig.java
+++ rest/src/main/java/gh/hack/rest/spring/SpringConfig.java
@@ -4,6 +4,6 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan("com.indian.experiment.*")
+@ComponentScan("gh.hack.*")
 public class SpringConfig {
 }
