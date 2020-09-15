083f5c41f17539e6ced28eba8efa4bebed9a2b8d
Pranav Saxena <psaxena130@gmail.com>
2020-09-15 22:40:57 +0530

    eventlisternhandler

--- rest/src/main/java/gh/hack/rest/handlers/EventListenHandler.java
+++ rest/src/main/java/gh/hack/rest/handlers/EventListenHandler.java
@@ -1,17 +1,31 @@
 package gh.hack.rest.handlers;
 
+import gh.hack.service.EventListenService;
+import io.netty.handler.codec.http.HttpResponseStatus;
 import io.vertx.core.Handler;
 import io.vertx.core.MultiMap;
 import io.vertx.ext.web.RoutingContext;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class EventListenHandler implements Handler<RoutingContext> {
+
+    @Autowired
+    private EventListenService eventListenService;
+
     @Override
     public void handle(RoutingContext event) {
         MultiMap multiMap = event.queryParams();
         String repoId = multiMap.get("repoId");
 
+        eventListenService.consumeEvent(repoId).setHandler(handler -> {
+            if(handler.succeeded()) {
+                event.response().setStatusCode(HttpResponseStatus.OK.code()).end();
+            } else {
+                event.response().setStatusCode(HttpResponseStatus.INTERNAL_SERVER_ERROR.code()).end();
+            }
+        });
 
     }
 }
