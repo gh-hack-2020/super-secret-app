5b3d8c82fdf1be23032ab2a00e3ec245144f4f46
Pranav Saxena <psaxena130@gmail.com>
2020-09-15 22:36:13 +0530

    lifx api integerated

--- data/src/main/java/gh/hack/data/external/api/calls/LifxApiCaller.java
+++ data/src/main/java/gh/hack/data/external/api/calls/LifxApiCaller.java
@@ -2,6 +2,7 @@ package gh.hack.data.external.api.calls;
 
 import io.vertx.core.Future;
 import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.client.WebClient;
 import io.vertx.ext.web.client.WebClientOptions;
 import org.springframework.stereotype.Component;
@@ -11,6 +12,8 @@ import javax.annotation.PostConstruct;
 @Component
 public class LifxApiCaller {
     private WebClient webClient;
+
+    private String authKey = "c2062372de4c5d007070fb7ded6e935c3b7e0c60acf2029a9fee39eb10c9fe5e";
 
     @PostConstruct
     public void init() {
@@ -26,6 +29,21 @@ public class LifxApiCaller {
 
     public Future<Void> glowBulb(String bulbId) {
         Future<Void> future = Future.future();
+        //Ref: https://api.lifx.com/v1/lights/:selector/state
+
+        JsonObject bodyJson = new JsonObject()
+                .put("power", "on")
+                .put("color", "red");
+
+        webClient.put("/v1/lights/" + bulbId + "/state")
+                .putHeader("Authorization", authKey)
+                .sendJsonObject(bodyJson, handler -> {
+                    if(handler.succeeded()) {
+                        future.complete();
+                    } else {
+                        future.fail(handler.cause());
+                    }
+                });
 
         return  future;
     }
