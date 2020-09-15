37449d8eafd4f63a22551f13e5fc764354aa2236
Pranav Saxena <psaxena130@gmail.com>
2020-09-15 21:13:43 +0530

    base of application is ready

---
+++ rest/src/main/java/gh/hack/rest/verticles/HttpVerticle.java
@@ -0,0 +1,66 @@
+package gh.hack.rest.verticles;
+
+import io.netty.handler.codec.http.HttpResponseStatus;
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.AsyncResult;
+import io.vertx.core.Future;
+import io.vertx.core.Handler;
+import io.vertx.core.http.HttpServer;
+import io.vertx.core.http.HttpServerOptions;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.handler.BodyHandler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class HttpVerticle extends AbstractVerticle {
+    private Logger logger = LoggerFactory.getLogger(HttpVerticle.class);
+
+
+    @Override
+    public void start(Future<Void> startFuture) throws Exception {
+        startWebApp(new Handler<AsyncResult<HttpServer>>() {
+            @Override
+            public void handle(AsyncResult<HttpServer> httpServerAsyncResult) {
+                completeStartup(httpServerAsyncResult, startFuture);
+            }
+        });
+    }
+
+    private void startWebApp(Handler<AsyncResult<HttpServer>> next) {
+        Router router = createRouter();
+        HttpServerOptions options = new HttpServerOptions();
+        options.setTcpKeepAlive(true);
+        vertx.createHttpServer(options).requestHandler(router::accept).exceptionHandler(
+                event -> {
+                    logger.error("Exception for request: error: {}", event);
+                }
+        ).listen(
+                config().getInteger("event.http.port", 8888), next
+        );
+    }
+
+    private void completeStartup(AsyncResult<HttpServer> http, Future<Void> fut) {
+        if (http.succeeded()) {
+            logger.info("Application is up");
+            fut.complete();
+        } else {
+            fut.fail(http.cause());
+        }
+    }
+
+    private Router createRouter() {
+        Router router = Router.router(vertx);
+        router.route().handler(BodyHandler.create());
+        getHealthCheck(router);
+        return router;
+    }
+
+    private void getHealthCheck(Router router) {
+        router.get("/health").handler(routingContext -> routingContext.response().setStatusCode(HttpResponseStatus.
+                OK.code()).end("{\"health\":\"true\"}"));
+    }
+
+}
